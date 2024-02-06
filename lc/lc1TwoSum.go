7217c902ed7ff83efcc2d7818f28ca9e5ac9f5ea
Stark <Shutaruku@example.com>
2024-02-06 14:28:44 +0800

    Add lc1 comment

--- lc/lc1TwoSum.go
+++ lc/lc1TwoSum.go
@@ -6,6 +6,8 @@ func twoSum(nums []int, target int) []int {
 
 	for i, ele := range nums {
 		if numMap[target-ele] != 0 {
+			// if htere esists 2 ele which sum is equal target
+			// then add idx to result
 			rst = append(rst, i)
 			rst = append(rst, numMap[target-ele]-1)
 			return rst
